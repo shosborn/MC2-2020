bcf7ac1d987760244f519b978978549c436f8c79
Tamal10 <stamal.buet@gmail.com>
2020-06-16 00:44:00 -0400

    experiment skeleton code

--- constants.py
+++ constants.py
@@ -40,8 +40,8 @@ class Constants:
                      'RLY': 'release_latency'
                      }
     '''
-    # overhead our name to csv file name map
-    overheadTypes = {
+
+    OVERHEAD_TYPES = {
                      'releaseLatency':'RLA', #delay until ISR starts execution
                      'ipiLatency':'IPI', #delay until ipi is received
                      'scheduling':'SCH', #process selection
@@ -62,3 +62,38 @@ class Constants:
     IS_DEDICATED_IRQ = False
 
     OVERHEAD_ACCOUNT = True
+
+    #related to sched study params
+
+    CORES_PER_COMPLEX = 4
+    NUM_CORES = 16
+    UTIL_STEP_SIZE = 0.2
+
+    MAX_SAMPLES = 2 #set at 2 for intial testing
+
+    LIGHT_RATIO = (0.1, 0.3)
+    MODERATE_RATIO = (0.1, 0.3)
+    HEAVY_RATIO = (0.5, 0.7)
+
+    CRITICALITY_UTIL_DIST = {
+        'A-Heavy':      [HEAVY_RATIO, LIGHT_RATIO, LIGHT_RATIO],
+        'B-Heavy':      [LIGHT_RATIO, HEAVY_RATIO, LIGHT_RATIO],
+        'C-Heavy':      [LIGHT_RATIO, LIGHT_RATIO, HEAVY_RATIO],
+        'AB-Moderate':  [MODERATE_RATIO, MODERATE_RATIO, LIGHT_RATIO],
+        'AC-Moderate':  [MODERATE_RATIO, LIGHT_RATIO, MODERATE_RATIO],
+        'BC-Moderate':  [LIGHT_RATIO, MODERATE_RATIO, MODERATE_RATIO],
+        'ALL-Moderate': [MODERATE_RATIO, MODERATE_RATIO, MODERATE_RATIO],
+    }
+
+    PERIOD_DIST = { #in ms (need to be in us?)
+        'Short':        [(3, 6), (6, 12), (3, 33)],
+        'Contrasting':  [(3, 6), (96, 192), (10, 100)],
+        'Long':         [(48, 96), (96, 192), (50, 500)]
+    }
+
+    TASK_UTIL = {
+        'Light':    [(0.001, 0.03), (0.001, 0.05), (0.001, 0.1)],
+        'Moderate': [(0.02, 0.1), (0.05, 0.2), (0.1, 0.4)],
+        'Heavy':    [(0.1, 0.2), (0.2, 0.4), (0.4, 0.6)]
+    }
+
---
+++ sched_study.py
@@ -0,0 +1,120 @@
+from constants import Constants
+from taskSystem import taskSystem
+import numpy as np
+import itertools
+import argparse
+
+def generateScenario():
+    paramList = {'utilDist':Constants.CRITICALITY_UTIL_DIST.keys(), 'period':Constants.PERIOD_DIST.keys(), 'taskUtil':Constants.TASK_UTIL.keys()}
+    keys = paramList.keys()
+    vals = paramList.values()
+    for instance in itertools.product(*vals):
+        yield dict(zip(keys, instance))
+
+def generateTaskSystem(utilDist,period,taskUtil,sysUtil):
+    # for now loading task system from sample file
+    totalCores = Constants.NUM_CORES
+    coresPerComplex = Constants.CORES_PER_COMPLEX
+    cacheSizeL3 = 2
+
+    assumedCache = cacheSizeL3
+    fileLevelA = "levelA-v1.csv"
+    fileLevelB = "levelB-v1.csv"
+    fileLevelC = "levelC-v1.csv"
+
+    mySystem = taskSystem(totalCores, coresPerComplex, cacheSizeL3, assumedCache,
+                          fileLevelA, fileLevelB, fileLevelC)
+    mySystem.levelA.loadSystem(fileLevelA)
+    mySystem.levelB.loadSystem(fileLevelB)
+    mySystem.levelC.loadSystem(fileLevelC)
+
+    return mySystem
+
+def schedStudySingleScenario(scenario,numCores,corePerComplex):
+    startUtil = 0
+    endUtil = 2 * numCores
+
+    for sysUtil in np.arange(startUtil, endUtil + Constants.UTIL_STEP_SIZE, Constants.UTIL_STEP_SIZE):
+        # assuming task sets are generated on the fly, otherwise read from csv file by loadSystem if pregenerated
+        # assuming a TaskSystem object is returned
+        numSamples = 0
+
+        #need to change it with some statsitical significance test?
+        while numSamples < Constants.MAX_SAMPLES:
+            taskSystem = generateTaskSystem(scenario['utilDist'], scenario['period'], scenario['taskUtil'], sysUtil)
+
+            #sched1: MC2-SMT-with-isolation
+            # taskSystem.levelA.loadSystem(fileLevelA)
+            taskSystem.levelA.setPairsList()
+            taskSystem.levelA.assignToCores(alg=Constants.WORST_FIT, coreList=taskSystem.platform.coreList)
+            print(taskSystem.levelA.schedulabilityTest(coreList=taskSystem.platform.coreList,
+                                                       allCritLevels=taskSystem.levels))
+            # mySystem.printPairsByCore()
+
+            # test level B
+            # taskSystem.levelB.loadSystem(fileLevelB)
+            taskSystem.levelB.setPairsList()
+            taskSystem.levelB.assignToCores(alg=Constants.WORST_FIT, coreList=taskSystem.platform.coreList)
+            print(taskSystem.levelB.schedulabilityTest(coreList=taskSystem.platform.coreList,
+                                                       allCritLevels=taskSystem.levels))
+
+            taskSystem.printPairsByCore()
+
+            # Test of level C
+            # taskSystem.levelC.loadSystem(fileLevelC)
+            taskSystem.levelC.decideThreaded()
+            # print solo tasks
+            '''print("Solo tasks:")
+            for t in taskSystem.levelC.soloTasks:
+                print(t.ID, t.currentSoloUtil)'''
+
+            '''print()
+            print("Threaded tasks:")
+            for t in taskSystem.levelC.threadedTasks:
+                print(t.ID, t.currentThreadedUtil)'''
+
+            taskSystem.levelC.divideCores(taskSystem.platform.coreList, corePerComplex)
+            '''print("Solo cores:")
+            for c in taskSystem.levelC.soloCores:
+                print(c.coreID)
+            print()
+            print("Threaded cores:")
+            for c in taskSystem.levelC.threadedCores:
+                print(c.coreID)'''
+
+            taskSystem.levelC.assignTasksToClusters()
+            taskSystem.printClusters()
+            print(taskSystem.levelC.schedulabilityTest(coreList=taskSystem.platform.coreList,
+                                                       allCritLevels=taskSystem.levels))
+
+            #todo: other schedulers to compare
+
+            #todo: update counters, mean, std dev, etc.
+
+        #todo: for this sysUtil write sched ratios to file
+
+    return
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('-d', "--datafile", type = argparse.FileType('w'),
+    # default = sys.stdout,
+    # help = "File to output csv file to")
+    parser.add_argument('-m', "--processors", default=Constants.NUM_CORES, type=int, help="Number of cores")
+    parser.add_argument('-c', "--corePerComplex", default=Constants.CORES_PER_COMPLEX, type=int, help="Number of cores per complex")
+    args = parser.parse_args()
+    numCores = args.processors
+    corePerComplex = args.corePerComplex
+
+    scenarios = generateScenario()
+
+    # to-do: parallel execution of scenarios?
+    for scenario in scenarios:
+        print(scenario)
+        schedStudySingleScenario(scenario,numCores,corePerComplex)
+
+    return
+
+if __name__ == '__main__':
+    main()
