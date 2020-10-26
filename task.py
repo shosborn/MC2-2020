e8c62aa5cea2e231bcbd71d30018c5badd65c42e
Joshua Bakita <jbakita@cs.unc.edu>
2020-10-25 21:44:32 -0400

    Fix a couple issues with Stephen's changes as noted by Shareef

--- task.py
+++ task.py
@@ -68,7 +68,7 @@ class Task:
         return distributions.sample_unif_distribution((tskmin,tskmax))
 
     def _scale_B_to_C(self, total_mc2_util: float) -> None:
-        wcet_to_avg = self._avg_percent(total_mc2_util - self._per_cache_crit_costs[Constants.CACHE_SIZE_L3, Constants.LEVEL_A])
+        wcet_to_avg = self._avg_percent(total_mc2_util - self.period * self._per_cache_crit_costs[Constants.CACHE_SIZE_L3, Constants.LEVEL_A])
         if self.level is Constants.LEVEL_A or self.level is Constants.LEVEL_B:
             for half_ways in range(Constants.MAX_HALF_WAYS+1):
                 self._per_cache_crit_costs[half_ways, Constants.LEVEL_C] = \
@@ -77,7 +77,7 @@ class Task:
             c_low = self._per_cache_crit_costs[Constants.MAX_HALF_WAYS, Constants.LEVEL_B]*wcet_to_avg
             c_high = self._per_cache_crit_costs[0, Constants.LEVEL_B]*wcet_to_avg
 
-            assert(c_high > c_low)
+            assert(c_high >= c_low)
 
             c_shared_min = c_low + distributions.sample_unif_distribution((0.3,0.7))*(c_high - c_low)
 
