bb0eb7c7398d828f23fa55e49f322c7b67a279bc
Joshua Bakita <jbakita@cs.unc.edu>
2021-04-08 14:20:56 -0400

    Output percent improvement in average SUA from aggregator.py + cleanup
    - Note that this is NOT average improvement in SUA over all scenarios!
      We use percent improvement in average SUA to avoid overweighting
      scenarios with very low schedulability (where the absolute change may
      be as low as 0.04, but this results in a 200%+ percent improvement).
      Note the value outputted by this should be the same as in Obs. 4 of
      the RTAS'21 paper.
    - Also remove log file gurobi.log which doesn't need to be in Git

--- aggregator.py
+++ aggregator.py
@@ -18,6 +18,7 @@ schemes = ['SOLO',
 def aggregate():
     res_files = os.listdir(results_path)
     writer = csv.DictWriter(open('aggregate.csv','w'),parameters + schemes)
+    suas = {scheme: [] for scheme in schemes}
     for rf in res_files:
         file_parameters = rf.split("__")
         write_row = {}
@@ -36,14 +37,29 @@ def aggregate():
             max_util = max([max_util, util])
             scenario_data['SOLO'][util] = float(row["NO_THREAD"])
             scenario_data['THREADED'][util] = float(row["THREAD_COURSE"])
-        #Compute areas by trapezoid rule
+        # Compute areas by trapezoid rule
         for util in scenario_data['SOLO'].keys():
             for scheme in schemes:
                 if util == max_util or util == min_util:
                     write_row[scheme] += 0.5*scenario_data[scheme][util]
                 else:
                     write_row[scheme] += scenario_data[scheme][util]
+        for scheme in schemes:
+            suas[scheme].append(write_row[scheme])
         writer.writerow(write_row)
+    # Note that we compute the % improvement in average SUAs, NOT the average %
+    # improvement in SUAs. This calculation approach avoids overweighting
+    # scenarios where the SUA changes very little in absolute terms, but a lot
+    # in percentage terms (such as where SUA goes from 0.01 to 0.05).
+    last_scheme = ""
+    last_avg_sua = 0
+    for scheme in schemes:
+        our_avg_sua = sum(suas[scheme])/len(suas[scheme])
+        print("Average", scheme, "SUA:", our_avg_sua)
+        if last_avg_sua != 0:
+            print("Average", scheme, "SUA is a", (our_avg_sua - last_avg_sua) / last_avg_sua * 100, "percent improvement over average", last_scheme, "SUA");
+        last_avg_sua = our_avg_sua
+        last_scheme = scheme
 
 if __name__ == "__main__":
     aggregate()
